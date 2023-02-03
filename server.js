9bc4873329c8f935eb0266e39f78c3a7d322c8c5
SALONI <smahajan7767@gmail.com>
2023-02-03 17:42:36 +0530

    simple node

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var exp = require('express');
 var path = require('path');
 var app = exp();
 
-var server = app.listen(9000,function()
+var server = app.listen(9000,function(req,res)
 {
+    var host = server.address().address
+    var port = server.address().port
    console.log("server created at 9000");
 });
 
@@ -11,7 +13,7 @@ app.get("/",function(req,res)
 {
    var person = {name:"saloni"};
    res.send(person);
-})
+});
 
 app.use(express.static(path.join(__dirname,'public')));
 
