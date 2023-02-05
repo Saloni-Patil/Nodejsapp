e68e7d779af245157a2f5f7ab161c2a9d4819073
SALONI <smahajan7767@gmail.com>
2023-02-05 20:12:53 +0530

    updated today

--- server.js
+++ server.js
@@ -1,21 +1,45 @@
-var exp = require('express');
+// var exp = require('express');
+// var path = require('path');
+// var app = exp();
+
+// app.use(exp.static(path.join(__dirname,'public')));
+
+// var server = app.listen(9000,function(req,res)
+// {
+//     var host = server.address().address
+//     var port = server.address().port
+//    console.log("server created at 9000");
+// });
+
+// app.get("/",function(req,res)
+// {
+//    var person = {name:"saloni"};
+//    res.send(person);
+// });
+
+var express = require('express');
+
+var app= express();
 var path = require('path');
-var app = exp();
 
-app.use(exp.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'Public')));
 
-var server = app.listen(9000,function(req,res)
-{
+
+
+app.get('/',function(req,res){
+
+    var person = {fname:"salu"}
+
+    res.send(person);
+});
+
+var server = app.listen(9000,function(req,res){
+
     var host = server.address().address
     var port = server.address().port
-   console.log("server created at 9000");
-});
 
-app.get("/",function(req,res)
-{
-   var person = {name:"saloni"};
-   res.send(person);
-});
+    console.log("ahe ithecgh");
 
+});
 
 
